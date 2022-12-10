873bb5a102903e8f9c43d8f977be16352b6aa433
Dmitriy <timon@yahoo.com>
2022-12-10 20:12:52 +0300

    Задание 41

---
+++ work6_41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41. Подсчитать кол-во чисел больше 0
+
+Console.Write("Сколько чисел Вы хотите ввести? ");
+int n = int.Parse(Console.ReadLine());
+int i = 0;
+int s = 0;
+while (i < n)
+{
+    Console.WriteLine("Введите " + (i + 1) + " число: ");
+    int a = int.Parse(Console.ReadLine());
+    if (a > 0)
+    {
+        i++;
+        s++;
+    }
+    else
+        i++;
+}
+
+Console.WriteLine("Количество чисел больше 0: " + s);
